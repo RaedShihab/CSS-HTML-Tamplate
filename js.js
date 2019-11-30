b20f1905538c5c3931ec9ab8776815f8a6f0c875
RaedShihab <raedshihab92@gmail.com>
2019-11-30 18:51:40 +0200

    add navbar

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
 
-if(localStorage.getItem('name') == null) {
-    const name = prompt('Add name');
-    localStorage.setItem('name', name)
-}
+// if(localStorage.getItem('name') == null) {
+//     const name = prompt('Add name');
+//     localStorage.setItem('name', name)
+// }
 
-const getName = localStorage.getItem('name')
+// const getName = localStorage.getItem('name')
 
- document.getElementById('id').innerHTML = getName
+//  document.getElementById('id').innerHTML = getName
 
